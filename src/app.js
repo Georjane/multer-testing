f86a21060b3d97c592e35394c34574a1906c2f6c
Georjane <pzgeorjane@gmail.com>
2021-02-08 19:55:37 +0100

    Configure diskstorage

--- src/app.js
+++ src/app.js
@@ -5,3 +5,12 @@ const multer = require('multer');
 app.listen(3000 || process.env.PORT, () => {
   console.log("Server on...");
 });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
