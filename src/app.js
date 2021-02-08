563b6a1db72b4c473ec7068e4b3e15fce272580d
Georjane <pzgeorjane@gmail.com>
2021-02-08 20:04:24 +0100

    Provide upload route

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const multer = require('multer');
 
+// Starting express server
 app.listen(3000 || process.env.PORT, () => {
   console.log("Server on...");
 });
 
+// Configure diskstorage. DiskStorage is a method made available by multer where 
+// we configure the destination of the file, 
+// the name of the file and 
+// we can also add validations regarding the type of the file.
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/");
@@ -14,3 +19,13 @@ const storage = multer.diskStorage({
     cb(null, Date.now() + "-" + file.originalname);
   },
 });
+
+// Providing an upload route
+const uploadStorage = multer({ storage: storage});
+
+//single files
+app.post('/upload/single', uploadStorage.single('file'), (req, res) => {
+  console.log(req.file);
+  return res.send('Single file');
+});
+
